c270bd758c37e5c18030c2e88d5d7fc93ba0b512
Arun Sambyal <arunsambyal123216av@gmail.com>
2026-07-12 23:29:05 +0530

    Implemented SecurityConfig

--- user-service/src/main/java/com/aifitness/userservice/Security/JWT/AuthenticationFilter.java
+++ user-service/src/main/java/com/aifitness/userservice/Security/JWT/AuthenticationFilter.java
@@ -1,10 +1,89 @@
 package com.aifitness.userservice.Security.JWT;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
+import jakarta.servlet.http.Cookie;
+import com.aifitness.userservice.Security.CustomUserDetailService;
+
+
 
 @Component
 public class AuthenticationFilter extends OncePerRequestFilter{
+    
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
+    private static final String Access_Token_Cookie = "access_token";
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private CustomUserDetailService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try{
+            String jwt = extractJwtFromRequest(request);
+            if(jwt != null && jwtService.validateToken(jwt)){
+
+                String email = jwtService.extractEmail(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+                if(jwtService.isTokenValid(email, userDetails)){
+
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+
+
+                }
+
+            }
+        }catch(Exception e){
+            logger.error("Could not set user authentication in security context", e);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractJwtFromRequest(HttpServletRequest request){
+
+        Cookie[] cookies = request.getCookies();
+
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                if(Access_Token_Cookie.equals(cookie.getName())){
+                    String token  = cookie.getValue();
+                    if(StringUtils.hasText(token)){
+                        return token;
+                    }
+                }
+            }
+
+        }
+        
+
+        //for postman testing
+        String bearerToken = request.getHeader("Authorization");
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
 
 }
---
+++ user-service/src/main/java/com/aifitness/userservice/Security/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.aifitness.userservice.Security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import com.aifitness.userservice.Security.JWT.AuthenticationFilter;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
+
+@EnableWebSecurity
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private AuthenticationFilter authenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("api/auth/register",
+                                "api/auth/login",
+                                "api/auth/refresh",
+                                "actuator/**"
+                ).permitAll()
+                .requestMatchers("api/users/all").hasRole("ADMIN")
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+}
+
